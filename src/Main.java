fd4170c02b80d44cd22b7ff1fb32e85ad886eca7
NohSungBeen <98868520+SungKong00@users.noreply.github.com>
2025-04-30 22:14:54 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import test.GitTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        GitTest gitTest = new GitTest();
+
+        gitTest.print_message();
+
+    }
+}
---
+++ src/test/GitTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class GitTest {
+
+    private String message = "change message.";
+
+    // 기능을 추가하고 git 명령어를 사용해 보세요.
+    public void print_message(){
+        System.out.println(message);
+    }
+}
